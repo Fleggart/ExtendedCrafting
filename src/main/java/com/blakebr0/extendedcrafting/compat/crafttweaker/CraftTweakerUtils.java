247ef61f1b4b304b1dd18b054da687c7975d53e2
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-25 13:10:51 +0800

    Clean up CraftTweakerUtils by removing NBT methods
    Removed unused methods for NBT handling.

--- src/main/java/com/blakebr0/extendedcrafting/compat/crafttweaker/CraftTweakerUtils.java
+++ src/main/java/com/blakebr0/extendedcrafting/compat/crafttweaker/CraftTweakerUtils.java
@@ -1,29 +1,16 @@
 package com.blakebr0.extendedcrafting.compat.crafttweaker;
 
-import crafttweaker.api.data.IData;
 import crafttweaker.api.item.IIngredient;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.nbt.NBTBase;
 import net.minecraft.util.NonNullList;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class CraftTweakerUtils {
-
-    @Nonnull
-	public static String writeTag(@Nullable NBTBase nbt) {
-		return Objects.toString(from(nbt));
-	}
-
-	@Nullable
-	private static IData from(@Nullable NBTBase nbt) {
-        return CraftTweakerMC.getIData(nbt);
-    }
 
     @Nonnull
     public static NonNullList<Ingredient> toIngredients(IIngredient... ingredients) {
