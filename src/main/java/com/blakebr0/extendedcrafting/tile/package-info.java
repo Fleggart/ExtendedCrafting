f186e28f25d843bdbe76fb892130a3009a8cfd49
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-25 11:22:18 +0800

    Delete src/main/java/com/blakebr0/extendedcrafting/tile/package-info.java

--- src/main/java/com/blakebr0/extendedcrafting/tile/package-info.java
+++
@@ -1,7 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package com.blakebr0.extendedcrafting.tile;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
