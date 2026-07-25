10f4dbb38f816cc5577e3620a05d882064f0d7ae
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-25 11:22:05 +0800

    Delete src/main/java/com/blakebr0/extendedcrafting/item/package-info.java

--- src/main/java/com/blakebr0/extendedcrafting/item/package-info.java
+++
@@ -1,7 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package com.blakebr0.extendedcrafting.item;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
