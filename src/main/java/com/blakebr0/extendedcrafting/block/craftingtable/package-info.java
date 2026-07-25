5924de33614d92d59135be2482446dfa491c2919
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-25 11:23:21 +0800

    Delete src/main/java/com/blakebr0/extendedcrafting/block/craftingtable/package-info.java

--- src/main/java/com/blakebr0/extendedcrafting/block/craftingtable/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package com.blakebr0.extendedcrafting.block.craftingtable;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
