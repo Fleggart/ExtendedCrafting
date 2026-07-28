05ebb700469694230adc8fc707e70ffab0ec6107
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-28 20:23:59 +0800

    Update ItemMaterial.java

--- src/main/java/com/blakebr0/extendedcrafting/item/ItemMaterial.java
+++ src/main/java/com/blakebr0/extendedcrafting/item/ItemMaterial.java
@@ -18,41 +18,23 @@ import java.util.Set;
 
 public class ItemMaterial extends ItemMeta {
 	
-	// 移除了所有Nugget的元数据：1, 25, 33, 37, 41, 49, 128, 129, 140
-	// 保留：13, 19, 32, 40, 48
-	public static final Set<Integer> ultimateMetas = new HashSet<>(Arrays.asList(13, 19, 32, 48));
+	public static final Set<Integer> ultimateMetas = new HashSet<>(Arrays.asList(13, 19));
 
-	// ===== 基础材料 =====
 	public static ItemStack itemBlackIron;
 	public static ItemStack itemBlackIronSlate;
-	public static ItemStack itemBlackIronRod;
-
 	public static ItemStack itemLuminessence;
 
-	// ===== 催化剂 =====
 	public static ItemStack itemBasicCatalyst;
 	public static ItemStack itemAdvancedCatalyst;
 	public static ItemStack itemEliteCatalyst;
 	public static ItemStack itemUltimateCatalyst;
-	public static ItemStack itemCrystaltineCatalyst;
-	public static ItemStack itemTheUltimateCatalyst;
 
-	// ===== 组件 =====
 	public static ItemStack itemBasicComponent;
 	public static ItemStack itemAdvancedComponent;
 	public static ItemStack itemEliteComponent;
 	public static ItemStack itemUltimateComponent;
-	public static ItemStack itemCrystaltineComponent;
-	public static ItemStack itemTheUltimateComponent;
 
-	// ===== 锭 =====
 	public static ItemStack itemCrystaltineIngot;
-	public static ItemStack itemTheUltimateIngot;
-	public static ItemStack itemEnderIngot;
-	public static ItemStack itemEnhancedEnderIngot;
-
-	// ===== 星 =====
-	public static ItemStack itemEnderStar;
 
 	public ItemMaterial() {
 		super("ec.material", ExtendedCrafting.REGISTRY);
@@ -61,37 +43,21 @@ public class ItemMaterial extends ItemMeta {
 
 	@Override
 	public void init() {
-		// 黑铁系列
 		itemBlackIron = addItem(0, "black_iron", "ingotBlackIron");
 		itemBlackIronSlate = addItem(2, "black_iron_slate");
-		itemBlackIronRod = addItem(3, "black_iron_rod");
-
 		itemLuminessence = addItem(7, "luminessence");
 
-		// 催化剂
 		itemBasicCatalyst = addItem(8, "basic_catalyst");
 		itemAdvancedCatalyst = addItem(9, "advanced_catalyst");
 		itemEliteCatalyst = addItem(10, "elite_catalyst");
 		itemUltimateCatalyst = addItem(11, "ultimate_catalyst");
-		itemCrystaltineCatalyst = addItem(12, "crystaltine_catalyst");
-		itemTheUltimateCatalyst = addItem(13, "ultimater_catalyst");
 
-		// 组件
 		itemBasicComponent = addItem(14, "basic_component");
 		itemAdvancedComponent = addItem(15, "advanced_component");
 		itemEliteComponent = addItem(16, "elite_component");
 		itemUltimateComponent = addItem(17, "ultimate_component");
-		itemCrystaltineComponent = addItem(18, "crystaltine_component");
-		itemTheUltimateComponent = addItem(19, "ultimater_component");
 
-		// 锭
 		itemCrystaltineIngot = addItem(24, "crystaltine_ingot", "ingotCrystaltine");
-		itemTheUltimateIngot = addItem(32, "ultimate_ingot", "ingotUltimate");
-		itemEnderIngot = addItem(36, "ender_ingot");
-		itemEnhancedEnderIngot = addItem(48, "enhanced_ender_ingot");
-
-		// 星
-		itemEnderStar = addItem(40, "ender_star");
 	}
 	
 	@Override
@@ -113,9 +79,6 @@ public class ItemMaterial extends ItemMeta {
 			break;
 		case 19:
 			tooltip.add(Colors.ITALICS + Utils.localize("tooltip.ec.ultimate_component"));
-			break;
-		case 32:
-			tooltip.add(Colors.ITALICS + Utils.localize("tooltip.ec.ultimate_ingot"));
 			break;
 		default:
 			break;
