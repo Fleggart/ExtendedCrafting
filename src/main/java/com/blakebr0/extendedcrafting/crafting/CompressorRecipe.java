3250393838d5cca6222fa38f3b0d7ac7434ff633
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-30 19:53:04 +0800

    Update CompressorRecipe.java

--- src/main/java/com/blakebr0/extendedcrafting/crafting/CompressorRecipe.java
+++ src/main/java/com/blakebr0/extendedcrafting/crafting/CompressorRecipe.java
@@ -9,21 +9,17 @@ public class CompressorRecipe {
 	protected final ItemStack output;
 	protected final Ingredient input;
 	protected final int inputCount;
-	protected final Ingredient catalyst;
-	protected final boolean consumeCatalyst;
 	protected final int powerCost;
 	protected final int powerRate;
 	
-	public CompressorRecipe(ItemStack output, Ingredient input, int inputCount, Ingredient catalyst, boolean consumeCatalyst, int powerCost) {
-		this(output, input, inputCount, catalyst, consumeCatalyst, powerCost, ModConfig.confCompressorRFRate);
+	public CompressorRecipe(ItemStack output, Ingredient input, int inputCount, int powerCost) {
+		this(output, input, inputCount, powerCost, ModConfig.confCompressorRFRate);
 	}
 
-	public CompressorRecipe(ItemStack output, Ingredient input, int inputCount, Ingredient catalyst, boolean consumeCatalyst, int powerCost, int powerRate) {
+	public CompressorRecipe(ItemStack output, Ingredient input, int inputCount, int powerCost, int powerRate) {
 		this.output = output;
 		this.input = input;
 		this.inputCount = inputCount;
-		this.catalyst = catalyst;
-		this.consumeCatalyst = consumeCatalyst;
 		this.powerCost = powerCost;
 		this.powerRate = powerRate;
 	}
@@ -38,14 +34,6 @@ public class CompressorRecipe {
 
 	public int getInputCount() {
 		return this.inputCount;
-	}
-
-	public Ingredient getCatalyst() {
-		return this.catalyst;
-	}
-
-	public boolean consumeCatalyst() {
-		return this.consumeCatalyst;
 	}
 
 	public int getPowerCost() {
