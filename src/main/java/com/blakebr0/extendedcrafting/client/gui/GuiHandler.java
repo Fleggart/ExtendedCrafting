c443c094eaff03ed6e3787a0e3c8755f9bfadfe9
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-30 14:00:14 +0800

    Update GuiHandler.java

--- src/main/java/com/blakebr0/extendedcrafting/client/gui/GuiHandler.java
+++ src/main/java/com/blakebr0/extendedcrafting/client/gui/GuiHandler.java
@@ -13,17 +13,12 @@ import net.minecraftforge.fml.common.network.IGuiHandler;
 public class GuiHandler implements IGuiHandler {
 
 	public static final int CRAFTING_CORE = 0;
-
 	public static final int AUTOMATION_INTERFACE = 6;
-
 	public static final int BASIC_TABLE = 7;
 	public static final int ADVANCED_TABLE = 8;
 	public static final int ELITE_TABLE = 9;
 	public static final int ULTIMATE_TABLE = 10;
-
 	public static final int COMPRESSOR = 13;
-
-	public static final int ENDER_CRAFTER = 19;
 
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
