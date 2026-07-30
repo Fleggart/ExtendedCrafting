cc7cb43efd9822606876dd3c3a294f2432c6e11c
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-30 17:47:03 +0800

    Refactor ModBlocks to include missing block registrations

--- src/main/java/com/blakebr0/extendedcrafting/block/ModBlocks.java
+++ src/main/java/com/blakebr0/extendedcrafting/block/ModBlocks.java
@@ -11,30 +11,27 @@ import com.blakebr0.extendedcrafting.lib.AddonReferenced;
 @AddonReferenced
 public class ModBlocks {
 
-	public static final BlockFrame blockFrame = new BlockFrame();
+    public static final BlockFrame blockFrame = new BlockFrame();
 
-	public static final BlockPedestal blockPedestal = new BlockPedestal();
-	@AddonReferenced public static final BlockCraftingCore blockCraftingCore = new BlockCraftingCore();
-	
-	public static final BlockAutomationInterface blockAutomationInterface = new BlockAutomationInterface();
 
-	@AddonReferenced public static final BlockBasicTable blockBasicTable = new BlockBasicTable();
-	@AddonReferenced public static final BlockAdvancedTable blockAdvancedTable = new BlockAdvancedTable();
-	@AddonReferenced public static final BlockEliteTable blockEliteTable = new BlockEliteTable();
-	@AddonReferenced public static final BlockUltimateTable blockUltimateTable = new BlockUltimateTable();
 
-	public static final BlockCompressor blockCompressor = new BlockCompressor();
+    public static final BlockAutomationInterface blockAutomationInterface = new BlockAutomationInterface();
 
-	public static void init() {
-		final ModRegistry registry = ExtendedCrafting.REGISTRY;
-		registry.register(blockFrame, "frame");
-		registry.register(blockPedestal, "pedestal");
-		registry.register(blockCraftingCore, "crafting_core");
-		registry.register(blockAutomationInterface, "interface");
-		registry.register(blockBasicTable, "table_basic");
-		registry.register(blockAdvancedTable, "table_advanced");
-		registry.register(blockEliteTable, "table_elite");
-		registry.register(blockUltimateTable, "table_ultimate");
-		registry.register(blockCompressor, "compressor");
-	}
+    @AddonReferenced public static final BlockBasicTable blockBasicTable = new BlockBasicTable();
+    @AddonReferenced public static final BlockAdvancedTable blockAdvancedTable = new BlockAdvancedTable();
+    @AddonReferenced public static final BlockEliteTable blockEliteTable = new BlockEliteTable();
+    @AddonReferenced public static final BlockUltimateTable blockUltimateTable = new BlockUltimateTable();
+
+    public static final BlockCompressor blockCompressor = new BlockCompressor();
+
+    public static void init() {
+        final ModRegistry registry = ExtendedCrafting.REGISTRY;
+        registry.register(blockFrame, "frame");
+        registry.register(blockAutomationInterface, "interface");
+        registry.register(blockBasicTable, "table_basic");
+        registry.register(blockAdvancedTable, "table_advanced");
+        registry.register(blockEliteTable, "table_elite");
+        registry.register(blockUltimateTable, "table_ultimate");
+        registry.register(blockCompressor, "compressor");
+    }
 }
