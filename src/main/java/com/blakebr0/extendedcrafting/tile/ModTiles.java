8744765903b7b90706fe8a4ca57fefef2c8fb35b
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-30 17:33:20 +0800

    Update ModTiles.java

--- src/main/java/com/blakebr0/extendedcrafting/tile/ModTiles.java
+++ src/main/java/com/blakebr0/extendedcrafting/tile/ModTiles.java
@@ -8,29 +8,22 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModTiles {
 
-	// 1.13, correct the namespaces
-	@SuppressWarnings("deprecation") // out of scope
-	public static void init() {
-		if (ModConfig.confCraftingCoreEnabled) {
-			GameRegistry.registerTileEntity(TilePedestal.class, "EC_Pedestal");
-			GameRegistry.registerTileEntity(TileCraftingCore.class, "EC_Crafting_Core");
-		}
+    @SuppressWarnings("deprecation")
+    public static void init() {
 
-		if (ModConfig.confInterfaceEnabled) {
-			GameRegistry.registerTileEntity(TileAutomationInterface.class, "EC_Automation_Interface");
-		}
+        if (ModConfig.confInterfaceEnabled) {
+            GameRegistry.registerTileEntity(TileAutomationInterface.class, "EC_Automation_Interface");
+        }
 
-		if (ModConfig.confTableEnabled) {
-			GameRegistry.registerTileEntity(TileBasicCraftingTable.class, "EC_Basic_Table");
-			GameRegistry.registerTileEntity(TileAdvancedCraftingTable.class, "EC_Advanced_Table");
-			GameRegistry.registerTileEntity(TileEliteCraftingTable.class, "EC_Elite_Table");
-			GameRegistry.registerTileEntity(TileUltimateCraftingTable.class, "EC_Ultimate_Table");
-		}
+        if (ModConfig.confTableEnabled) {
+            GameRegistry.registerTileEntity(TileBasicCraftingTable.class, "EC_Basic_Table");
+            GameRegistry.registerTileEntity(TileAdvancedCraftingTable.class, "EC_Advanced_Table");
+            GameRegistry.registerTileEntity(TileEliteCraftingTable.class, "EC_Elite_Table");
+            GameRegistry.registerTileEntity(TileUltimateCraftingTable.class, "EC_Ultimate_Table");
+        }
 
-		if (ModConfig.confCompressorEnabled) {
-			GameRegistry.registerTileEntity(TileCompressor.class, "EC_Compressor");
-		}
-		
-		// TileEnderCrafter 已移除
-	}
+        if (ModConfig.confCompressorEnabled) {
+            GameRegistry.registerTileEntity(TileCompressor.class, "EC_Compressor");
+        }
+    }
 }
