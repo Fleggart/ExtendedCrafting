791b8e780e3ffb96a6b4cb748644e0eefb64f828
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-30 17:56:08 +0800

    Update CompatJEI.java

--- src/main/java/com/blakebr0/extendedcrafting/compat/jei/CompatJEI.java
+++ src/main/java/com/blakebr0/extendedcrafting/compat/jei/CompatJEI.java
@@ -5,18 +5,14 @@ import com.blakebr0.extendedcrafting.Tags;
 import com.blakebr0.extendedcrafting.block.ModBlocks;
 import com.blakebr0.extendedcrafting.client.container.*;
 import com.blakebr0.extendedcrafting.client.gui.*;
-import com.blakebr0.extendedcrafting.compat.jei.combinationcrafting.CombinationCraftingCategory;
-import com.blakebr0.extendedcrafting.compat.jei.combinationcrafting.CombinationCraftingWrapper;
+
 import com.blakebr0.extendedcrafting.compat.jei.compressor.CompressorCraftingCategory;
 import com.blakebr0.extendedcrafting.compat.jei.compressor.CompressorCraftingWrapper;
-// 删除这一行: import com.blakebr0.extendedcrafting.compat.jei.endercrafter.EnderCrafterCategory;
 import com.blakebr0.extendedcrafting.compat.jei.tablecrafting.*;
 import com.blakebr0.extendedcrafting.config.ModConfig;
-import com.blakebr0.extendedcrafting.crafting.CombinationRecipe;
-import com.blakebr0.extendedcrafting.crafting.CombinationRecipeManager;
+
 import com.blakebr0.extendedcrafting.crafting.CompressorRecipe;
 import com.blakebr0.extendedcrafting.crafting.CompressorRecipeManager;
-// 删除这一行: import com.blakebr0.extendedcrafting.crafting.endercrafter.EnderCrafterRecipeManager;
 import com.blakebr0.extendedcrafting.crafting.table.TableRecipeManager;
 import com.blakebr0.extendedcrafting.crafting.table.TableRecipeShaped;
 import com.blakebr0.extendedcrafting.crafting.table.TableRecipeShapeless;
@@ -36,88 +32,76 @@ import java.util.List;
 @JEIPlugin
 public class CompatJEI implements IModPlugin {
 
-	public static final ResourceLocation ICONS = ResourceHelper.getResource(Tags.MODID, "textures/jei/icons.png");
-	
-	public static final List<Block> blocks = new ArrayList<>();
-	public static final List<Item> items = new ArrayList<>();
-	
-	@Override
-	public void registerCategories(IRecipeCategoryRegistration registry) {
-		IGuiHelper helper = registry.getJeiHelpers().getGuiHelper();
-		
-		if (ModConfig.confCraftingCoreEnabled) {
-			registry.addRecipeCategories(new CombinationCraftingCategory(helper));
-		}
-		
-		if (ModConfig.confTableEnabled) {
-			registry.addRecipeCategories(new BasicTableCategory(helper));
-			registry.addRecipeCategories(new AdvancedTableCategory(helper));
-			registry.addRecipeCategories(new EliteTableCategory(helper));
-			registry.addRecipeCategories(new UltimateTableCategory(helper));
-		}
+    public static final ResourceLocation ICONS = ResourceHelper.getResource(Tags.MODID, "textures/jei/icons.png");
 
-		if (ModConfig.confCompressorEnabled) {
-			registry.addRecipeCategories(new CompressorCraftingCategory(helper));
-		}
-		
-		// 删除整个 if (ModConfig.confEnderEnabled) 块
-	}
-	
-	@Override
-	public void register(IModRegistry registry) {
-		blocks.forEach(block -> registry.addIngredientInfo(new ItemStack(block), VanillaTypes.ITEM, "desc." + block.getTranslationKey()));
-		items.forEach(item -> registry.addIngredientInfo(new ItemStack(item), VanillaTypes.ITEM, "desc." + item.getTranslationKey()));
-		
-		IJeiHelpers helpers = registry.getJeiHelpers();
-		IRecipeTransferRegistry transfer = registry.getRecipeTransferRegistry();
-		
-		if (ModConfig.confCraftingCoreEnabled) {
-			registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockCraftingCore), CombinationCraftingCategory.UID);
-			registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockPedestal), CombinationCraftingCategory.UID);
-			registry.handleRecipes(CombinationRecipe.class, recipe -> new CombinationCraftingWrapper(helpers, recipe), CombinationCraftingCategory.UID);
-			registry.addRecipeClickArea(GuiCraftingCore.class, 117, 47, 21, 14, CombinationCraftingCategory.UID);
-			registry.addRecipes(CombinationRecipeManager.getInstance().getRecipes(), CombinationCraftingCategory.UID);
-		}
-		
-		if (ModConfig.confTableEnabled) {
-			registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockBasicTable), BasicTableCategory.UID);
-			registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockAdvancedTable), AdvancedTableCategory.UID);
-			registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockEliteTable), EliteTableCategory.UID);
-			registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockUltimateTable), UltimateTableCategory.UID);
-			registry.handleRecipes(TableRecipeShaped.class, recipe -> new TableShapedWrapper(helpers, recipe, 217, 0), BasicTableCategory.UID);
-			registry.handleRecipes(TableRecipeShapeless.class, recipe -> new TableShapelessWrapper(helpers, recipe, 215, 0), BasicTableCategory.UID);
-			registry.handleRecipes(TableRecipeShaped.class, recipe -> new TableShapedWrapper(helpers, recipe, 285, 0), AdvancedTableCategory.UID);
-			registry.handleRecipes(TableRecipeShapeless.class, recipe -> new TableShapelessWrapper(helpers, recipe, 283, 0), AdvancedTableCategory.UID);
-			registry.handleRecipes(TableRecipeShaped.class, recipe -> new TableShapedWrapper(helpers, recipe, 237, 257), EliteTableCategory.UID);
-			registry.handleRecipes(TableRecipeShapeless.class, recipe -> new TableShapelessWrapper(helpers, recipe, 235, 257), EliteTableCategory.UID);
-			registry.handleRecipes(TableRecipeShaped.class, recipe -> new TableShapedWrapper(helpers, recipe, 308, 329), UltimateTableCategory.UID);
-			registry.handleRecipes(TableRecipeShapeless.class, recipe -> new TableShapelessWrapper(helpers, recipe, 306, 329), UltimateTableCategory.UID);
-			registry.addRecipeClickArea(GuiBasicTable.class, 91, 37, 21, 14, BasicTableCategory.UID);
-			registry.addRecipeClickArea(GuiAdvancedTable.class, 109, 54, 21, 14, AdvancedTableCategory.UID);
-			registry.addRecipeClickArea(GuiEliteTable.class, 139, 72, 21, 14, EliteTableCategory.UID);
-			registry.addRecipeClickArea(GuiUltimateTable.class, 174, 90, 21, 14, UltimateTableCategory.UID);
-			registry.addRecipes(TableRecipeManager.getInstance().getRecipesTiered(1), BasicTableCategory.UID);
-			registry.addRecipes(TableRecipeManager.getInstance().getRecipesTiered(2), AdvancedTableCategory.UID);
-			registry.addRecipes(TableRecipeManager.getInstance().getRecipesTiered(3), EliteTableCategory.UID);
-			registry.addRecipes(TableRecipeManager.getInstance().getRecipesTiered(4), UltimateTableCategory.UID);
-			transfer.addRecipeTransferHandler(ContainerBasicTable.class, BasicTableCategory.UID, 1, 9, 10, 36);
-			transfer.addRecipeTransferHandler(ContainerAdvancedTable.class, AdvancedTableCategory.UID, 1, 25, 26, 36);
-			transfer.addRecipeTransferHandler(ContainerEliteTable.class, EliteTableCategory.UID, 1, 49, 50, 36);
-			transfer.addRecipeTransferHandler(ContainerUltimateTable.class, UltimateTableCategory.UID, 1, 81, 82, 36);
-			
-			if (ModConfig.confTableUseRecipes) {
-				registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockBasicTable), VanillaRecipeCategoryUid.CRAFTING);
-				transfer.addRecipeTransferHandler(ContainerBasicTable.class, VanillaRecipeCategoryUid.CRAFTING, 1, 9, 10, 36);
-			}
-		}
+    public static final List<Block> blocks = new ArrayList<>();
+    public static final List<Item> items = new ArrayList<>();
 
-		if (ModConfig.confCompressorEnabled) {
-			registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockCompressor), CompressorCraftingCategory.UID);
-			registry.handleRecipes(CompressorRecipe.class, CompressorCraftingWrapper::new, CompressorCraftingCategory.UID);
-			registry.addRecipeClickArea(GuiCompressor.class, 97, 47, 21, 14, CompressorCraftingCategory.UID);
-			registry.addRecipes(CompressorRecipeManager.getInstance().getRecipes(), CompressorCraftingCategory.UID);
-		}
-		
-		// 删除整个 if (ModConfig.confEnderEnabled) 块
-	}
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registry) {
+        IGuiHelper helper = registry.getJeiHelpers().getGuiHelper();
+
+
+
+        if (ModConfig.confTableEnabled) {
+            registry.addRecipeCategories(new BasicTableCategory(helper));
+            registry.addRecipeCategories(new AdvancedTableCategory(helper));
+            registry.addRecipeCategories(new EliteTableCategory(helper));
+            registry.addRecipeCategories(new UltimateTableCategory(helper));
+        }
+
+        if (ModConfig.confCompressorEnabled) {
+            registry.addRecipeCategories(new CompressorCraftingCategory(helper));
+        }
+    }
+
+    @Override
+    public void register(IModRegistry registry) {
+        blocks.forEach(block -> registry.addIngredientInfo(new ItemStack(block), VanillaTypes.ITEM, "desc." + block.getTranslationKey()));
+        items.forEach(item -> registry.addIngredientInfo(new ItemStack(item), VanillaTypes.ITEM, "desc." + item.getTranslationKey()));
+
+        IJeiHelpers helpers = registry.getJeiHelpers();
+        IRecipeTransferRegistry transfer = registry.getRecipeTransferRegistry();
+
+
+
+        if (ModConfig.confTableEnabled) {
+            registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockBasicTable), BasicTableCategory.UID);
+            registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockAdvancedTable), AdvancedTableCategory.UID);
+            registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockEliteTable), EliteTableCategory.UID);
+            registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockUltimateTable), UltimateTableCategory.UID);
+            registry.handleRecipes(TableRecipeShaped.class, recipe -> new TableShapedWrapper(helpers, recipe, 217, 0), BasicTableCategory.UID);
+            registry.handleRecipes(TableRecipeShapeless.class, recipe -> new TableShapelessWrapper(helpers, recipe, 215, 0), BasicTableCategory.UID);
+            registry.handleRecipes(TableRecipeShaped.class, recipe -> new TableShapedWrapper(helpers, recipe, 285, 0), AdvancedTableCategory.UID);
+            registry.handleRecipes(TableRecipeShapeless.class, recipe -> new TableShapelessWrapper(helpers, recipe, 283, 0), AdvancedTableCategory.UID);
+            registry.handleRecipes(TableRecipeShaped.class, recipe -> new TableShapedWrapper(helpers, recipe, 237, 257), EliteTableCategory.UID);
+            registry.handleRecipes(TableRecipeShapeless.class, recipe -> new TableShapelessWrapper(helpers, recipe, 235, 257), EliteTableCategory.UID);
+            registry.handleRecipes(TableRecipeShaped.class, recipe -> new TableShapedWrapper(helpers, recipe, 308, 329), UltimateTableCategory.UID);
+            registry.handleRecipes(TableRecipeShapeless.class, recipe -> new TableShapelessWrapper(helpers, recipe, 306, 329), UltimateTableCategory.UID);
+            registry.addRecipeClickArea(GuiBasicTable.class, 91, 37, 21, 14, BasicTableCategory.UID);
+            registry.addRecipeClickArea(GuiAdvancedTable.class, 109, 54, 21, 14, AdvancedTableCategory.UID);
+            registry.addRecipeClickArea(GuiEliteTable.class, 139, 72, 21, 14, EliteTableCategory.UID);
+            registry.addRecipeClickArea(GuiUltimateTable.class, 174, 90, 21, 14, UltimateTableCategory.UID);
+            registry.addRecipes(TableRecipeManager.getInstance().getRecipesTiered(1), BasicTableCategory.UID);
+            registry.addRecipes(TableRecipeManager.getInstance().getRecipesTiered(2), AdvancedTableCategory.UID);
+            registry.addRecipes(TableRecipeManager.getInstance().getRecipesTiered(3), EliteTableCategory.UID);
+            registry.addRecipes(TableRecipeManager.getInstance().getRecipesTiered(4), UltimateTableCategory.UID);
+            transfer.addRecipeTransferHandler(ContainerBasicTable.class, BasicTableCategory.UID, 1, 9, 10, 36);
+            transfer.addRecipeTransferHandler(ContainerAdvancedTable.class, AdvancedTableCategory.UID, 1, 25, 26, 36);
+            transfer.addRecipeTransferHandler(ContainerEliteTable.class, EliteTableCategory.UID, 1, 49, 50, 36);
+            transfer.addRecipeTransferHandler(ContainerUltimateTable.class, UltimateTableCategory.UID, 1, 81, 82, 36);
+
+            if (ModConfig.confTableUseRecipes) {
+                registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockBasicTable), VanillaRecipeCategoryUid.CRAFTING);
+                transfer.addRecipeTransferHandler(ContainerBasicTable.class, VanillaRecipeCategoryUid.CRAFTING, 1, 9, 10, 36);
+            }
+        }
+
+        if (ModConfig.confCompressorEnabled) {
+            registry.addRecipeCatalyst(new ItemStack(ModBlocks.blockCompressor), CompressorCraftingCategory.UID);
+            registry.handleRecipes(CompressorRecipe.class, CompressorCraftingWrapper::new, CompressorCraftingCategory.UID);
+            registry.addRecipeClickArea(GuiCompressor.class, 97, 47, 21, 14, CompressorCraftingCategory.UID);
+            registry.addRecipes(CompressorRecipeManager.getInstance().getRecipes(), CompressorCraftingCategory.UID);
+        }
+    }
 }
