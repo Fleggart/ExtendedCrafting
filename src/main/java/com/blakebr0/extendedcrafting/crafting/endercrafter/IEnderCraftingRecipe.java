c1f347aa1acefa0cd4864373af1277531d06fc8b
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-30 14:24:33 +0800

    Delete src/main/java/com/blakebr0/extendedcrafting/crafting/endercrafter/IEnderCraftingRecipe.java9

--- src/main/java/com/blakebr0/extendedcrafting/crafting/endercrafter/IEnderCraftingRecipe.java
+++
@@ -1,7 +0,0 @@
-package com.blakebr0.extendedcrafting.crafting.endercrafter;
-
-import net.minecraft.item.crafting.IRecipe;
-
-public interface IEnderCraftingRecipe extends IRecipe {
-	int getEnderCrafterTimeSeconds();
-}
