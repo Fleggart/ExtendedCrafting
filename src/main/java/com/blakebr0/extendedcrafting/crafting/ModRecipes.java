0e01a8432089161a7269e2487aeabd5480f3a35c
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-30 14:30:26 +0800

    Update ModRecipes.java

--- src/main/java/com/blakebr0/extendedcrafting/crafting/ModRecipes.java
+++ src/main/java/com/blakebr0/extendedcrafting/crafting/ModRecipes.java
@@ -1,7 +1,5 @@
 package com.blakebr0.extendedcrafting.crafting;
 
-import com.blakebr0.extendedcrafting.config.ModConfig;
-import com.blakebr0.extendedcrafting.crafting.endercrafter.EnderCrafterRecipeManager;
 import com.blakebr0.extendedcrafting.crafting.table.TableRecipeManager;
 import com.blakebr0.extendedcrafting.item.ItemMaterial;
 import com.blakebr0.extendedcrafting.item.ModItems;
@@ -11,24 +9,24 @@ import net.minecraftforge.common.crafting.CraftingHelper;
 
 public class ModRecipes {
 
-	public static void init() {
-		TableRecipeManager.getInstance().addShaped(ItemMaterial.itemCrystaltineIngot,
-				CraftingHelper.parseShaped(
-						"DLLLLLD",
-						"DNIGIND",
-						"DNIGIND",
-						"DLLLLLD",
+    public static void init() {
+        TableRecipeManager.getInstance().addShaped(ItemMaterial.itemCrystaltineIngot,
+                CraftingHelper.parseShaped(
+                        "DLLLLLD",
+                        "DNIGIND",
+                        "DNIGIND",
+                        "DLLLLLD",
 
-						'D', "gemDiamond",
-						'L', new ItemStack(Items.DYE, 1, 4),
-						'N', Items.NETHER_STAR,
-						'I', "ingotIron",
-						'G', "ingotGold"
-				)
-		);
+                        'D', "gemDiamond",
+                        'L', new ItemStack(Items.DYE, 1, 4),
+                        'N', Items.NETHER_STAR,
+                        'I', "ingotIron",
+                        'G', "ingotGold"
+                )
+        );
 
-		ModItems.itemSingularity.initRecipes();
-		ModItems.itemSingularityUltimate.initRecipe();
-		ModItems.itemSingularityCustom.initRecipes();
-	}
+        ModItems.itemSingularity.initRecipes();
+        ModItems.itemSingularityUltimate.initRecipe();
+        ModItems.itemSingularityCustom.initRecipes();
+    }
 }
