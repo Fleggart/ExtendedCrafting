13353caea7608503515018442a3208850a8e77be
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-31 20:18:55 +0800

    Update ModItems.java

--- src/main/java/com/blakebr0/extendedcrafting/item/ModItems.java
+++ src/main/java/com/blakebr0/extendedcrafting/item/ModItems.java
@@ -2,28 +2,22 @@ package com.blakebr0.extendedcrafting.item;
 
 import com.blakebr0.cucumber.registry.ModRegistry;
 import com.blakebr0.extendedcrafting.ExtendedCrafting;
+import com.blakebr0.extendedcrafting.config.ModConfig;
+import com.blakebr0.extendedcrafting.lib.AddonReferenced;
 
+@AddonReferenced
 public class ModItems {
-    
-    // 删除这行
-    // public static final ItemGuide itemGuide = new ItemGuide("extendedcrafting", ExtendedCrafting.CREATIVE_TAB, ModGuide.GUIDE);
 
     public static final ItemMaterial itemMaterial = new ItemMaterial();
-
     public static final ItemSingularity itemSingularity = new ItemSingularity();
-    public static final ItemSingularityCustom itemSingularityCustom = new ItemSingularityCustom();
     public static final ItemSingularityUltimate itemSingularityUltimate = new ItemSingularityUltimate();
+    public static final ItemSingularityCustom itemSingularityCustom = new ItemSingularityCustom();
 
     public static void init() {
         final ModRegistry registry = ExtendedCrafting.REGISTRY;
-        
-        // 删除这行
-        // registry.register(itemGuide, "guide");
-
         registry.register(itemMaterial, "material");
-
         registry.register(itemSingularity, "singularity");
-        registry.register(itemSingularityCustom, "singularity_custom");
         registry.register(itemSingularityUltimate, "singularity_ultimate");
+        registry.register(itemSingularityCustom, "singularity_custom");
     }
 }
