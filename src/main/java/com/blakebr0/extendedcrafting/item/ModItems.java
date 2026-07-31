6aa1eacf61ad1ecf74f2302b7d7cdfb57525ecff
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-31 20:49:43 +0800

    Update ModItems.java

--- src/main/java/com/blakebr0/extendedcrafting/item/ModItems.java
+++ src/main/java/com/blakebr0/extendedcrafting/item/ModItems.java
@@ -7,15 +7,13 @@ import com.blakebr0.extendedcrafting.lib.AddonReferenced;
 
 @AddonReferenced
 public class ModItems {
-
-    public static final ItemMaterial itemMaterial = new ItemMaterial();
     public static final ItemSingularity itemSingularity = new ItemSingularity();
     public static final ItemSingularityUltimate itemSingularityUltimate = new ItemSingularityUltimate();
     public static final ItemSingularityCustom itemSingularityCustom = new ItemSingularityCustom();
 
     public static void init() {
         final ModRegistry registry = ExtendedCrafting.REGISTRY;
-        registry.register(itemMaterial, "material");
+        
         registry.register(itemSingularity, "singularity");
         registry.register(itemSingularityUltimate, "singularity_ultimate");
         registry.register(itemSingularityCustom, "singularity_custom");
