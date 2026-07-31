58bf1a738d8f0931eb093e9d0f73633b1a5b894d
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-31 23:01:09 +0800

    Update ModBlocks.java

--- src/main/java/com/blakebr0/extendedcrafting/block/ModBlocks.java
+++ src/main/java/com/blakebr0/extendedcrafting/block/ModBlocks.java
@@ -10,23 +10,15 @@ import com.blakebr0.extendedcrafting.lib.AddonReferenced;
 
 @AddonReferenced
 public class ModBlocks {
-
-    public static final BlockFrame blockFrame = new BlockFrame();
-
-
-
     public static final BlockAutomationInterface blockAutomationInterface = new BlockAutomationInterface();
-
     @AddonReferenced public static final BlockBasicTable blockBasicTable = new BlockBasicTable();
     @AddonReferenced public static final BlockAdvancedTable blockAdvancedTable = new BlockAdvancedTable();
     @AddonReferenced public static final BlockEliteTable blockEliteTable = new BlockEliteTable();
     @AddonReferenced public static final BlockUltimateTable blockUltimateTable = new BlockUltimateTable();
 
     public static final BlockCompressor blockCompressor = new BlockCompressor();
-
     public static void init() {
         final ModRegistry registry = ExtendedCrafting.REGISTRY;
-        registry.register(blockFrame, "frame");
         registry.register(blockAutomationInterface, "interface");
         registry.register(blockBasicTable, "table_basic");
         registry.register(blockAdvancedTable, "table_advanced");
