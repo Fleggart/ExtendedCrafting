ea2822b6c87f493b2f3f01d3f4d188b5c902e4c8
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-31 22:09:30 +0800

    Create Tags.java

---
+++ src/main/java/com/blakebr0/extendedcrafting/Tags.java
@@ -0,0 +1,7 @@
+package com.blakebr0.extendedcrafting;
+
+public class Tags {
+    public static final String MODID = "extendedcrafting";
+    public static final String MODNAME = "Extended Crafting";
+    public static final String VERSION = "1.0.0";
+}
