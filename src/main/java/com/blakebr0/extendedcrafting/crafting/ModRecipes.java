cde12fdcbf4be69c7a3fd4df4a34484bb059b279
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-02 03:15:44 +0800

    Remove ultimate singularity recipe initialization

--- src/main/java/com/blakebr0/extendedcrafting/crafting/ModRecipes.java
+++ src/main/java/com/blakebr0/extendedcrafting/crafting/ModRecipes.java
@@ -1,15 +1,10 @@
 package com.blakebr0.extendedcrafting.crafting;
 
-import com.blakebr0.extendedcrafting.crafting.table.TableRecipeManager;
 import com.blakebr0.extendedcrafting.item.ModItems;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.crafting.CraftingHelper;
 
 public class ModRecipes {
     public static void init() {
         ModItems.itemSingularity.initRecipes();
-        ModItems.itemSingularityUltimate.initRecipe();
         ModItems.itemSingularityCustom.initRecipes();
     }
 }
