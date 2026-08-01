a9c7e7fc985e4cf5a97412bbcca5cb1683faf79a
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-02 03:13:44 +0800

    Update ItemSingularity.java

--- src/main/java/com/blakebr0/extendedcrafting/item/ItemSingularity.java
+++ src/main/java/com/blakebr0/extendedcrafting/item/ItemSingularity.java
@@ -10,15 +10,12 @@ import com.blakebr0.extendedcrafting.crafting.CompressorRecipeManager;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.init.Items;
 import net.minecraft.item.EnumRarity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
 import net.minecraftforge.common.crafting.CraftingHelper;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -52,7 +49,6 @@ public class ItemSingularity extends ItemMeta implements IEnableable {
 
     @Override
     public void init() {
-        // 保留默认奇点定义
         addSingularity(0, "coal", new ItemStack(Items.COAL), 0x1B1B1B);
         addSingularity(1, "iron", "ingotIron", 0x969696);
         addSingularity(2, "lapis_lazuli", new ItemStack(Items.DYE, 1, 4), 0x345EC3);
@@ -62,31 +58,31 @@ public class ItemSingularity extends ItemMeta implements IEnableable {
         addSingularity(6, "diamond", "gemDiamond", 0x2CCDB1);
         addSingularity(7, "emerald", "gemEmerald", 0x00A835);
         addSingularity(16, "aluminum", "ingotAluminum", 0xCACCDA);
-		addSingularity(17, "copper", "ingotCopper", 0xCE7201);
-		addSingularity(18, "tin", "ingotTin", 0x7690A5);
-		addSingularity(19, "bronze", "ingotBronze", 0xA87544);
-		addSingularity(20, "zinc", "ingotZinc", 0xCFD2CC);
-		addSingularity(21, "brass", "ingotBrass", 0xBC8B22);
-		addSingularity(22, "silver", "ingotSilver", 0x83AAB2);
-		addSingularity(23, "lead", "ingotLead", 0x484F67);
-		addSingularity(24, "steel", "ingotSteel", 0x565656);
-		addSingularity(25, "nickel", "ingotNickel", 0xBEB482);
-		addSingularity(26, "constantan", "ingotConstantan", 0xA98544);
-		addSingularity(27, "electrum", "ingotElectrum", 0xA79135);
-		addSingularity(28, "invar", "ingotInvar", 0x929D97);
-		addSingularity(29, "mithril", "ingotMithril", 0x659ABB);
-		addSingularity(30, "tungsten", "ingotTungsten", 0x494E51);
-		addSingularity(31, "titanium", "ingotTitanium", 0xA6A7B8);
-		addSingularity(32, "uranium", "ingotUranium", 0x46800D);
-		addSingularity(33, "chrome", "ingotChrome", 0xC1A9AE);
-		addSingularity(34, "platinum", "ingotPlatinum", 0x6FEAEF);
-		addSingularity(35, "iridium", "ingotIridium", 0x949FBE);
-		addSingularity(48, "signalum", "ingotSignalum", 0xDD3B00);
-		addSingularity(49, "lumium", "ingotLumium", 0xDEE59C);
-		addSingularity(50, "enderium", "ingotEnderium", 0x0B4D4E);
-		addSingularity(64, "ardite", "ingotArdite", 0xDA4817);
-		addSingularity(65, "cobalt", "ingotCobalt", 0x023C9B);
-		addSingularity(66, "manyullyn", "ingotManyullyn", 0x5C268A);
+        addSingularity(17, "copper", "ingotCopper", 0xCE7201);
+        addSingularity(18, "tin", "ingotTin", 0x7690A5);
+        addSingularity(19, "bronze", "ingotBronze", 0xA87544);
+        addSingularity(20, "zinc", "ingotZinc", 0xCFD2CC);
+        addSingularity(21, "brass", "ingotBrass", 0xBC8B22);
+        addSingularity(22, "silver", "ingotSilver", 0x83AAB2);
+        addSingularity(23, "lead", "ingotLead", 0x484F67);
+        addSingularity(24, "steel", "ingotSteel", 0x565656);
+        addSingularity(25, "nickel", "ingotNickel", 0xBEB482);
+        addSingularity(26, "constantan", "ingotConstantan", 0xA98544);
+        addSingularity(27, "electrum", "ingotElectrum", 0xA79135);
+        addSingularity(28, "invar", "ingotInvar", 0x929D97);
+        addSingularity(29, "mithril", "ingotMithril", 0x659ABB);
+        addSingularity(30, "tungsten", "ingotTungsten", 0x494E51);
+        addSingularity(31, "titanium", "ingotTitanium", 0xA6A7B8);
+        addSingularity(32, "uranium", "ingotUranium", 0x46800D);
+        addSingularity(33, "chrome", "ingotChrome", 0xC1A9AE);
+        addSingularity(34, "platinum", "ingotPlatinum", 0x6FEAEF);
+        addSingularity(35, "iridium", "ingotIridium", 0x949FBE);
+        addSingularity(48, "signalum", "ingotSignalum", 0xDD3B00);
+        addSingularity(49, "lumium", "ingotLumium", 0xDEE59C);
+        addSingularity(50, "enderium", "ingotEnderium", 0x0B4D4E);
+        addSingularity(64, "ardite", "ingotArdite", 0xDA4817);
+        addSingularity(65, "cobalt", "ingotCobalt", 0x023C9B);
+        addSingularity(66, "manyullyn", "ingotManyullyn", 0x5C268A);
     }
 
     @Override
@@ -109,7 +105,7 @@ public class ItemSingularity extends ItemMeta implements IEnableable {
         if (enabled) {
             singularityColors.put(meta, color);
             singularityMaterials.put(meta, material);
-            ItemSingularityUltimate.addSingularityToRecipe(new ItemStack(this, 1, meta));
+            // 已删除: ItemSingularityUltimate.addSingularityToRecipe(new ItemStack(this, 1, meta));
         }
 
         addItem(meta, name, enabled);
@@ -122,7 +118,7 @@ public class ItemSingularity extends ItemMeta implements IEnableable {
         if (enabled) {
             singularityColors.put(meta, color);
             singularityMaterials.put(meta, oreName);
-            ItemSingularityUltimate.addSingularityToRecipe(new ItemStack(this, 1, meta));
+            // 已删除: ItemSingularityUltimate.addSingularityToRecipe(new ItemStack(this, 1, meta));
         }
 
         addItem(meta, name, enabled);
