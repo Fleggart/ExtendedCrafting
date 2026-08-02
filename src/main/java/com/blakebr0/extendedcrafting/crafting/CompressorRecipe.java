08e96711208f2cd50253c7dcf9f6fb0b8b5b6953
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-02 20:50:04 +0800

    Update CompressorRecipe.java

--- src/main/java/com/blakebr0/extendedcrafting/crafting/CompressorRecipe.java
+++ src/main/java/com/blakebr0/extendedcrafting/crafting/CompressorRecipe.java
@@ -1,6 +1,5 @@
 package com.blakebr0.extendedcrafting.crafting;
 
-import com.blakebr0.extendedcrafting.config.ModConfig;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 
@@ -10,18 +9,12 @@ public class CompressorRecipe {
 	protected final Ingredient input;
 	protected final int inputCount;
 	protected final int powerCost;
-	protected final int powerRate;
 	
 	public CompressorRecipe(ItemStack output, Ingredient input, int inputCount, int powerCost) {
-		this(output, input, inputCount, powerCost, ModConfig.confCompressorRFRate);
-	}
-
-	public CompressorRecipe(ItemStack output, Ingredient input, int inputCount, int powerCost, int powerRate) {
 		this.output = output;
 		this.input = input;
 		this.inputCount = inputCount;
 		this.powerCost = powerCost;
-		this.powerRate = powerRate;
 	}
 
 	public ItemStack getOutput() {
@@ -40,7 +33,4 @@ public class CompressorRecipe {
 		return this.powerCost;
 	}
 	
-	public int getPowerRate() {
-		return this.powerRate;
-	}
 }
